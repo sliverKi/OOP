684dd6a8eabeeb64ee458d8f861998b22c341596
sliverKi <lovee2756@gmail.com>
2024-02-26 21:02:51 +0900

    Create Single ProductItem-class : render single item

--- JS-Basic/Remind/assets/scripts/app.js
+++ JS-Basic/Remind/assets/scripts/app.js
@@ -8,10 +8,33 @@ class Product {
     }
 }
 
+class ProductItem { //단일 상품에 대한 아웃 소싱
 
-const productList = {
+    constructor(product) { 
+        console.log("h")
+        this.product = product
+    }
+    render() { 
+        console.log("kk")
+        const prodEl = document.createElement('li')
+        prodEl.className='product-item'
+        prodEl.innerHTML = `
+        <div>
+            <img src=${this.product.imageUrl} alt=${this.product.title} />
+            <div class="prod-item__container">
+                <h2>${this.product.title}</h2>
+                <p>${this.product.price}</p>
+                <h3>${this.product.description}</h3>
+                <button>Add to Cart </button>
+            </div>
+        </div>
+        `;
+        return prodEl
+    }
+}
 
-    products : [//productList 배열 생성 --> [{객체1}, {객체2}, {개개체3}..{객체4}]
+class ProductList { 
+    products = [
         new Product(
             "apple",
             "https://parade.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTkwNTgxNDY1MzcxMTkxMTY0/different-types-of-apples-jpg.jpg",
@@ -24,33 +47,22 @@ const productList = {
             10000,
             "9 bunch of green apples",
         )   
-    ],
+    ]
 
     render() { //render method
-        console.log("say huio: ", this)
         const renderHook = document.getElementById('app')
         const prodList = document.createElement("ul")
         prodList.className = 'product-list'
         for (const prod of this.products) { 
-            const prodEl = document.createElement('li')
-            prodEl.className='product-item'
-            prodEl.innerHTML = `
-            <div>
-                <img src=${prod.imageUrl} alt=${prod.title} />
-                <div class="prod-item__container">
-                    <h2>${prod.title}</h2>
-                    <p>${prod.price}</p>
-                    <h3>${prod.description}</h3>
-                    <button>Add to Cart </button>
-                </div>
-            </div>
-            `;
+            const prodItem = new ProductItem(prod)//단일 상품 아웃소싱
+            const prodEl = prodItem.render()
             prodList.append(prodEl)
         }
         renderHook.append(prodList)
     }
 }
 
-productList.render()
+const prodList = new ProductList()
+prodList.render()
 
 
